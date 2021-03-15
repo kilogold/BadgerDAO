6776af90a0258142488029f828e8a0586d1ac958
Kilogold <bonilla_kelvin@hotmail.com>
2021-03-14 22:34:13 -0700

    Updating contract to ERC20 transactions

--- Contracts/Faucet.sol
+++ Contracts/Faucet.sol
@@ -1,49 +1,45 @@
 // SPDX-License-Identifier: CC-BY-SA-4.0
 
 // Version of Solidity compiler this program was written for
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.2;
 
-// Our first contract is a faucet!
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+// My first contract is a faucet! :D
 contract Faucet 
 {
+    ERC20 constant internal FAUCET_TOKEN = ERC20(0xFab46E002BbF0b4509813474841E0716E6730136);
+
     mapping (address => uint256) private participants;
     uint256 private participantRetryTime = 3 seconds;
     
-    constructor(uint256 retryAmount) public
+    constructor(uint256 retryAmount) payable
     {
         participantRetryTime = retryAmount;
     }
     
     receive () external payable {}
     
-    function donate() public payable{}
-    
-    // Give out ether to anyone who asks
-    function withdraw(uint withdraw_amount) public 
+    function grant(address recipient, uint256 amount) public
     {
         require(canParticipate());
         
-        // Limit withdrawal amount
-        require(withdraw_amount <= address(this).balance);
-
-        participants[msg.sender] = now;
+        require(amount <= FAUCET_TOKEN.balanceOf(address(this)));
         
-        // Send the amount to the address that requested it
-        msg.sender.transfer(withdraw_amount);
-    }
-    
-    function getBalance() public view returns (uint256)
-    {
-        return address(this).balance;
+        //TODO:Check for gas
+                
+        require(FAUCET_TOKEN.transfer(recipient, amount));
+        
+        participants[msg.sender]  = block.timestamp;
     }
     
     function canParticipate() public view returns (bool)
     {
-        return(participants[msg.sender] + participantRetryTime < now);
+        return(participants[msg.sender] + participantRetryTime < block.timestamp);
     }
     
     function getElapsedTime() public view returns (uint256)
     {
-        return(now - participants[msg.sender]);
+        return(block.timestamp - participants[msg.sender]);
     }
 }
