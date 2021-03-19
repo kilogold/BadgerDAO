8d98213df424b082189792263925abb7081d6bbb
Kilogold <bonilla_kelvin@hotmail.com>
2021-03-18 18:44:45 -0700

    Monving things around

---
+++ Contracts/Faucet/FaucetService.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Numerics;
+using Nethereum.Hex.HexTypes;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Web3;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Contracts.CQS;
+using Nethereum.Contracts.ContractHandlers;
+using Nethereum.Contracts;
+using System.Threading;
+using Contracts.Contracts.Faucet.ContractDefinition;
+
+namespace Contracts.Contracts.Faucet
+{
+    public partial class FaucetService
+    {
+        public static Task<TransactionReceipt> DeployContractAndWaitForReceiptAsync(Nethereum.Web3.Web3 web3, FaucetDeployment faucetDeployment, CancellationTokenSource cancellationTokenSource = null)
+        {
+            return web3.Eth.GetContractDeploymentHandler<FaucetDeployment>().SendRequestAndWaitForReceiptAsync(faucetDeployment, cancellationTokenSource);
+        }
+
+        public static Task<string> DeployContractAsync(Nethereum.Web3.Web3 web3, FaucetDeployment faucetDeployment)
+        {
+            return web3.Eth.GetContractDeploymentHandler<FaucetDeployment>().SendRequestAsync(faucetDeployment);
+        }
+
+        public static async Task<FaucetService> DeployContractAndGetServiceAsync(Nethereum.Web3.Web3 web3, FaucetDeployment faucetDeployment, CancellationTokenSource cancellationTokenSource = null)
+        {
+            var receipt = await DeployContractAndWaitForReceiptAsync(web3, faucetDeployment, cancellationTokenSource);
+            return new FaucetService(web3, receipt.ContractAddress);
+        }
+
+        protected Nethereum.Web3.Web3 Web3{ get; }
+
+        public ContractHandler ContractHandler { get; }
+
+        public FaucetService(Nethereum.Web3.Web3 web3, string contractAddress)
+        {
+            Web3 = web3;
+            ContractHandler = web3.Eth.GetContractHandler(contractAddress);
+        }
+
+        public Task<bool> CanParticipateQueryAsync(CanParticipateFunction canParticipateFunction, BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<CanParticipateFunction, bool>(canParticipateFunction, blockParameter);
+        }
+
+        
+        public Task<bool> CanParticipateQueryAsync(BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<CanParticipateFunction, bool>(null, blockParameter);
+        }
+
+        public Task<BigInteger> GetElapsedTimeQueryAsync(GetElapsedTimeFunction getElapsedTimeFunction, BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<GetElapsedTimeFunction, BigInteger>(getElapsedTimeFunction, blockParameter);
+        }
+
+        
+        public Task<BigInteger> GetElapsedTimeQueryAsync(BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<GetElapsedTimeFunction, BigInteger>(null, blockParameter);
+        }
+
+        public Task<string> GrantRequestAsync(GrantFunction grantFunction)
+        {
+             return ContractHandler.SendRequestAsync(grantFunction);
+        }
+
+        public Task<TransactionReceipt> GrantRequestAndWaitForReceiptAsync(GrantFunction grantFunction, CancellationTokenSource cancellationToken = null)
+        {
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(grantFunction, cancellationToken);
+        }
+
+        public Task<string> GrantRequestAsync(string recipient, BigInteger amount)
+        {
+            var grantFunction = new GrantFunction();
+                grantFunction.Recipient = recipient;
+                grantFunction.Amount = amount;
+            
+             return ContractHandler.SendRequestAsync(grantFunction);
+        }
+
+        public Task<TransactionReceipt> GrantRequestAndWaitForReceiptAsync(string recipient, BigInteger amount, CancellationTokenSource cancellationToken = null)
+        {
+            var grantFunction = new GrantFunction();
+                grantFunction.Recipient = recipient;
+                grantFunction.Amount = amount;
+            
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(grantFunction, cancellationToken);
+        }
+    }
+}
---
+++ Contracts/Token/ContractDefinition/TokenDefinition.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Numerics;
+using Nethereum.Hex.HexTypes;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Web3;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Contracts.CQS;
+using Nethereum.Contracts;
+using System.Threading;
+
+namespace Contracts.Contracts.Token.ContractDefinition
+{
+
+
+    public partial class TokenDeployment : TokenDeploymentBase
+    {
+        public TokenDeployment() : base(BYTECODE) { }
+        public TokenDeployment(string byteCode) : base(byteCode) { }
+    }
+
+    public class TokenDeploymentBase : ContractDeploymentMessage
+    {
+        public static string BYTECODE = "";
+        public TokenDeploymentBase() : base(BYTECODE) { }
+        public TokenDeploymentBase(string byteCode) : base(byteCode) { }
+
+    }
+
+    public partial class AllowanceFunction : AllowanceFunctionBase { }
+
+    [Function("allowance", "uint256")]
+    public class AllowanceFunctionBase : FunctionMessage
+    {
+        [Parameter("address", "_owner", 1)]
+        public virtual string Owner { get; set; }
+        [Parameter("address", "_spender", 2)]
+        public virtual string Spender { get; set; }
+    }
+
+    public partial class ApproveFunction : ApproveFunctionBase { }
+
+    [Function("approve", "bool")]
+    public class ApproveFunctionBase : FunctionMessage
+    {
+        [Parameter("address", "_spender", 1)]
+        public virtual string Spender { get; set; }
+        [Parameter("uint256", "_value", 2)]
+        public virtual BigInteger Value { get; set; }
+    }
+
+    public partial class BalanceOfFunction : BalanceOfFunctionBase { }
+
+    [Function("balanceOf", "uint256")]
+    public class BalanceOfFunctionBase : FunctionMessage
+    {
+        [Parameter("address", "_owner", 1)]
+        public virtual string Owner { get; set; }
+    }
+
+    public partial class TransferFunction : TransferFunctionBase { }
+
+    [Function("transfer", "bool")]
+    public class TransferFunctionBase : FunctionMessage
+    {
+        [Parameter("address", "_to", 1)]
+        public virtual string To { get; set; }
+        [Parameter("uint256", "_value", 2)]
+        public virtual BigInteger Value { get; set; }
+    }
+
+    public partial class TransferFromFunction : TransferFromFunctionBase { }
+
+    [Function("transferFrom", "bool")]
+    public class TransferFromFunctionBase : FunctionMessage
+    {
+        [Parameter("address", "_from", 1)]
+        public virtual string From { get; set; }
+        [Parameter("address", "_to", 2)]
+        public virtual string To { get; set; }
+        [Parameter("uint256", "_value", 3)]
+        public virtual BigInteger Value { get; set; }
+    }
+
+    public partial class ApprovalEventDTO : ApprovalEventDTOBase { }
+
+    [Event("Approval")]
+    public class ApprovalEventDTOBase : IEventDTO
+    {
+        [Parameter("address", "_owner", 1, true )]
+        public virtual string Owner { get; set; }
+        [Parameter("address", "_spender", 2, true )]
+        public virtual string Spender { get; set; }
+        [Parameter("uint256", "_value", 3, false )]
+        public virtual BigInteger Value { get; set; }
+    }
+
+    public partial class TransferEventDTO : TransferEventDTOBase { }
+
+    [Event("Transfer")]
+    public class TransferEventDTOBase : IEventDTO
+    {
+        [Parameter("address", "_from", 1, true )]
+        public virtual string From { get; set; }
+        [Parameter("address", "_to", 2, true )]
+        public virtual string To { get; set; }
+        [Parameter("uint256", "_value", 3, false )]
+        public virtual BigInteger Value { get; set; }
+    }
+
+    public partial class AllowanceOutputDTO : AllowanceOutputDTOBase { }
+
+    [FunctionOutput]
+    public class AllowanceOutputDTOBase : IFunctionOutputDTO 
+    {
+        [Parameter("uint256", "remaining", 1)]
+        public virtual BigInteger Remaining { get; set; }
+    }
+
+
+
+    public partial class BalanceOfOutputDTO : BalanceOfOutputDTOBase { }
+
+    [FunctionOutput]
+    public class BalanceOfOutputDTOBase : IFunctionOutputDTO 
+    {
+        [Parameter("uint256", "balance", 1)]
+        public virtual BigInteger Balance { get; set; }
+    }
+
+
+
+
+}
