1a8c0439db72b9f7b5aef120c1bb8f108b18488f
Kilogold <bonilla_kelvin@hotmail.com>
2021-04-24 10:53:13 -0700

    Documentation for workarounds

--- Brownie/contracts/BrownieEnvWrappers.sol
+++ Brownie/contracts/BrownieEnvWrappers.sol
@@ -1,3 +1,17 @@
+/*************************************************************************************************************
+* By using brownie's package manager, we exclude package symbols into compilation,
+* unless explicitly used by Solidity contracts codebase.
+* 
+* I suppose this is to optimize how many object get compiled into brownie's Python execution environment.
+* As a side effect, we are unable to access package symbols, unless we use a specific fixture:
+* https://eth-brownie.readthedocs.io/en/stable/package-manager.html#using-packages-in-tests
+* 
+* Although this resolves the limitation for tests, brownie scripts are still doomed.
+* If brownie tests depend on brownie scripts, then we're back to square-one.
+* To work around this latter limitation, we define our own symbols that wrap-around an existing packaged symbol
+* in order to include it in the compilation process (as stated initially above).
+*************************************************************************************************************/
+
 // SPDX-License-Identifier: CC-BY-SA-4.0
 
 // Version of Solidity compiler this program was written for
